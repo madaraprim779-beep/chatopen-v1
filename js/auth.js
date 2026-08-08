990218b2119f1eb16e8b9ff48034a361311b6e50
madaraprim779-beep <madaraprim779@gmail.com>
2026-08-08 20:32:31 +0000

    Create js/auth.js

---
+++ js/auth.js
@@ -0,0 +1,14 @@
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { auth } from "./firebase.js";
+
+export function requireAuth(callback) {
+  return onAuthStateChanged(auth, (user) => {
+
+    if (user) {
+      callback(user);
+    } else {
+      window.location.href = "login.html";
+    }
+
+  });
+}
