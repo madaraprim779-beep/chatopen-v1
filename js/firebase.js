7c1985435acfd3638fb9cd6331785eaa5a77f1a1
madaraprim779-beep <madaraprim779@gmail.com>
2026-08-09 17:44:01 +0000

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,3 +1,9 @@
+// js/firebase.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "...",
   authDomain: "...",
@@ -6,3 +12,9 @@ const firebaseConfig = {
   messagingSenderId: "...",
   appId: "..."
 };
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
