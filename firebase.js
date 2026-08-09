67a45223131b6edd63cc696f0c5b8a990619274d
madaraprim779-beep <madaraprim779@gmail.com>
2026-08-09 01:03:11 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,65 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+// ==========================================
+// CHATOPEN - FIREBASE
+// ==========================================
+
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+
+// ==========================================
+// CONFIGURATION FIREBASE
+// ==========================================
 
 const firebaseConfig = {
-  apiKey: "TON_API_KEY",
-  authDomain: "chatopen-af39a.firebaseapp.com",
-  projectId: "chatopen-af39a",
-  storageBucket: "chatopen-af39a.firebasestorage.app",
-  messagingSenderId: "378168580119",
-  appId: "1:378168580119:web:867eb1b9df244e09ec6452"
+
+  apiKey: "hkM5L8xisIWaUnEubHTq",
+
+  authDomain:
+    "chatopen-af39a.firebaseapp.com",
+
+  projectId:
+    "chatopen-af39a",
+
+  storageBucket:
+    "chatopen-af39a.firebasestorage.app",
+
+  messagingSenderId:
+    "378168580119",
+
+  appId:
+    "1:378168580119:web:867eb1b9df244e09ec6452"
+
 };
 
-const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// ==========================================
+// INITIALISATION
+// ==========================================
+
+const app =
+  initializeApp(firebaseConfig);
+
+
+// ==========================================
+// AUTHENTIFICATION
+// ==========================================
+
+export const auth =
+  getAuth(app);
+
+
+// ==========================================
+// FIRESTORE
+// ==========================================
+
+export const db =
+  getFirestore(app);
