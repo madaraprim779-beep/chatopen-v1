46ce2ea4698d467db8b5251ec14fb5cbc00eda23
madaraprim779-beep <madaraprim779@gmail.com>
2026-08-10 22:52:52 +0000

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,20 +1,50 @@
-// js/firebase.js
+// ==========================================
+// CHATOPEN - FIREBASE
+// ==========================================
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
+
+
+// ==========================================
+// CONFIGURATION FIREBASE
+// ==========================================
 
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyB3dUnrB7RathmCi4qCkbzeA9fKOKSBCoc",
+  authDomain: "chatopen-af39a.firebaseapp.com",
+  projectId: "chatopen-af39a",
+  storageBucket: "chatopen-af39a.firebasestorage.app",
+  messagingSenderId: "378168580119",
+  appId: "1:378168580119:web:867eb1b9df244e09ec6452"
 };
+
+
+// ==========================================
+// INITIALISATION
+// ==========================================
 
 const app = initializeApp(firebaseConfig);
 
+
+// ==========================================
+// AUTHENTIFICATION
+// ==========================================
+
 export const auth = getAuth(app);
+
+
+// ==========================================
+// FIRESTORE
+// ==========================================
+
 export const db = getFirestore(app);
-export default app;
