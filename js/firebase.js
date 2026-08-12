0016428155f2e5c4f9f2fc075746246f160e464c
madaraprim779-beep <madaraprim779@gmail.com>
2026-08-12 02:02:36 +0000

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,50 +1,36 @@
-// ==========================================
-// CHATOPEN - FIREBASE
-// ==========================================
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 
-import {
-  initializeApp
-} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+import { getAuth } from
+"https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 
-import {
-  getAuth
-} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
+import { getFirestore } from
+"https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 
-import {
-  getFirestore
-} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
-
-
-// ==========================================
-// CONFIGURATION FIREBASE
-// ==========================================
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3dUnrB7RathmCi4qCkbzeA9fKOKSBCoc",
-  authDomain: "chatopen-af39a.firebaseapp.com",
-  projectId: "chatopen-af39a",
-  storageBucket: "chatopen-af39a.firebasestorage.app",
-  messagingSenderId: "378168580119",
-  appId: "1:378168580119:web:867eb1b9df244e09ec6452"
+  apiKey: "TON_API_KEY",
+  authDomain: "TON_PROJECT_ID.firebaseapp.com",
+  projectId: "TON_PROJECT_ID",
+  storageBucket: "TON_PROJECT_ID.firebasestorage.app",
+  messagingSenderId: "TON_SENDER_ID",
+  appId: "TON_APP_ID"
 };
 
-
-// ==========================================
-// INITIALISATION
-// ==========================================
 
 const app = initializeApp(firebaseConfig);
 
 
-// ==========================================
-// AUTHENTIFICATION
-// ==========================================
-
-export const auth = getAuth(app);
+/* Firebase Authentication */
+const auth = getAuth(app);
 
 
-// ==========================================
-// FIRESTORE
-// ==========================================
+/* Cloud Firestore */
+const db = getFirestore(app);
 
-export const db = getFirestore(app);
+
+export {
+  app,
+  auth,
+  db
+};
